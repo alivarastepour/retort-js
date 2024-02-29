807aaf56a609846fa57894a9f0e40be02c05a30a
alivarastepour <alivaraste@outlook.com>
2024-02-29 11:55:25 +0330

    regex playground

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use serde_json::{from_str, Map, Value};
 
 // fn into_hashamp(map: &serde_json::Map<std::string::String, Value>) -> HashMap<String, String> {
@@ -12,33 +13,17 @@ use serde_json::{from_str, Map, Value};
 // }
 
 fn main() {
-    let path = "x".to_owned();
-
-    let state = String::from("{\"a\":6, \"b\":{\"c\":2,\"d\":11}}");
-    let map: serde_json::Map<std::string::String, Value> = from_str(&state).unwrap();
-    // let res = obj_key_path_to_value(map, path);
-    // println!("res: {res}")
-
-    // let state_object: Value = from_str(&state).unwrap;
-    // match state_object {
-    //     Value::Array(arr) => {
-    //         println!("arr")
-    //     }
-    //     Value::Bool(b) => {
-    //         println!("boo")
-    //     }
-    //     Value::Null => {
-    //         println!("null")
-    //     }
-    //     Value::Number(num) => {
-    //         println!("num")
-    //     }
-    //     Value::Object(obj) => {
-    //         let x = into_hashamp(&obj);
-    //         println!("{x}");
-    //     }
-    //     Value::String(st) => {
-    //         println!("st")
-    //     }
+    let markup = "<div onclick={someShit}>I am {age} years old</div>";
+    let markup_regex = Regex::new(">.*\\{(.*)\\}.*<").unwrap();
+    // let what = markup_regex.captures_iter(markup);
+    // let stuff = Regex::find(&markup_regex, markup).unwrap();
+    // let x = Regex::captures_iter(&markup_regex, markup);
+    // for item in x {
+    //     let ha: (&str, [&str; 1]) = item.extract();
+    //     let h1 = ha.0;
+    //     let x = ha.1[0];
+    //     println!("{x}")
     // }
+    let a = markup_regex.find(markup).unwrap();
+    println!("{:?}", a)
 }

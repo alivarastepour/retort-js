15c49c4e5843d81141664a3dea11cea5108be011
alivarastepour <alivaraste@outlook.com>
2024-03-19 23:21:58 +0330

    added prototype for component_did_mount effect

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,8 +1,10 @@
 pub mod component_mod {
+
     use crate::presenter::presenter_mod::Presenter;
     use serde::{Deserialize, Serialize};
     use serde_json::{Map, Value};
-    use wasm_bindgen::prelude::wasm_bindgen;
+    use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+    use web_sys::{console::log_1, js_sys::Function};
 
     #[derive(Serialize, Deserialize)]
     #[wasm_bindgen]
@@ -10,6 +12,8 @@ pub mod component_mod {
         state: String,
         presenter: Box<Presenter>,
         props: String,
+        #[serde(with = "serde_wasm_bindgen::preserve")]
+        component_did_mount: Function,
     }
 
     const NO_VALUE: &str = "undefined";
@@ -21,6 +25,7 @@ pub mod component_mod {
                 presenter: self.presenter.clone(),
                 props: self.props.to_owned(),
                 state: self.state.to_owned(),
+                component_did_mount: self.component_did_mount.to_owned(),
             }
         }
     }
@@ -28,12 +33,28 @@ pub mod component_mod {
     #[wasm_bindgen]
     impl Component {
         #[wasm_bindgen(constructor)]
-        pub fn new(state: String, presenter: Presenter, props: String) -> Component {
+        pub fn new(
+            state: String,
+            presenter: Presenter,
+            props: String,
+            component_did_mount: &Function,
+        ) -> Component {
             Component {
                 state,
                 presenter: Box::new(presenter),
                 props,
+                component_did_mount: component_did_mount.clone(),
             }
+        }
+
+        #[wasm_bindgen(getter)]
+        pub fn component_did_mount(&self) -> Function {
+            self.component_did_mount.clone()
+        }
+
+        #[wasm_bindgen(setter)]
+        pub fn set_component_did_mount(&mut self, component_did_mount: &Function) {
+            self.component_did_mount = component_did_mount.clone();
         }
 
         #[wasm_bindgen(getter)]
@@ -132,22 +153,48 @@ pub mod component_mod {
             val
         }
 
+        #[wasm_bindgen]
+        pub fn register_event_listener(
+            id: &str,
+            event_type: &str,
+            event_listener: &Function,
+        ) -> bool {
+            let window = web_sys::window().expect("where window?");
+            let document = window.document().expect("where document?");
+            let target = document.get_element_by_id(id).expect("where target?");
+            let result = target.add_event_listener_with_callback(event_type, event_listener);
+            match result {
+                Result::Err(_err) => false,
+                Result::Ok(_v) => true,
+            }
+        }
+
         pub fn render(&self) {
             let window = web_sys::window().expect("where window?");
             let document = window.document().expect("where document?");
             let root = document.get_element_by_id("root").expect("where root?");
+
             let presenter = &*self.presenter;
             match presenter {
                 Presenter::Component(component) => {
                     component.render();
                 }
                 Presenter::Markup(markup) => {
-                    let m = format!("<div>{markup}</div>");
+                    let m = format!("{markup}");
                     root.set_inner_html(&m);
                 }
                 Presenter::Nothing(_) => {
                     root.set_inner_html("<div>nothing</div>");
                 }
+            }
+            let component_js_value =
+                serde_wasm_bindgen::to_value(self).unwrap_or(JsValue::undefined());
+            let res = self.component_did_mount.call0(&component_js_value);
+            match res {
+                Result::Err(err) => {
+                    log_1(&err);
+                }
+                Result::Ok(_a) => {}
             }
         }
     }

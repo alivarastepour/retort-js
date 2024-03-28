3b02c1a879d089076e73220554e816d3c55e80c6
alivarastepour <alivaraste@outlook.com>
2024-03-28 23:41:36 +0330

    added a general struct for tokenizer util functions

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -1,7 +1,7 @@
 pub mod tokenizer_mod {
     use std::usize::MIN;
 
-    pub enum TokenizerCurrentState {
+    pub enum TokenizerState {
         Uninitialized,
         OpenAngleBracket,
         CloseAngleBracket,
@@ -15,7 +15,12 @@ pub mod tokenizer_mod {
         Error(String),
     }
 
-    impl Clone for TokenizerCurrentState {
+    pub struct CurrentState {
+        pub state: TokenizerState,
+        pub token: String,
+    }
+
+    impl Clone for TokenizerState {
         fn clone(&self) -> Self {
             match self {
                 Self::CloseAngleBracket => Self::CloseAngleBracket,
@@ -33,10 +38,7 @@ pub mod tokenizer_mod {
         }
     }
 
-    pub fn proceed_from_uninitialized(
-        markup: &Vec<char>,
-        index: &mut usize,
-    ) -> (String, TokenizerCurrentState) {
+    pub fn proceed_from_uninitialized(markup: &Vec<char>, index: &mut usize) -> CurrentState {
         let max = markup.len();
         loop {
             if *index == max {
@@ -45,17 +47,23 @@ pub mod tokenizer_mod {
             let current_string = markup[*index].to_string();
             let current = current_string.trim();
             if current == "<" {
-                return ("<".to_owned(), TokenizerCurrentState::OpenAngleBracket);
+                return CurrentState {
+                    token: "<".to_owned(),
+                    state: TokenizerState::OpenAngleBracket,
+                };
             } else if current != "" {
                 // TODO: handle error case?
-                return (
-                    markup.into_iter().collect(),
-                    TokenizerCurrentState::Finalized,
-                );
+                return CurrentState {
+                    token: markup.into_iter().collect(),
+                    state: TokenizerState::Finalized,
+                };
             }
             *index = *index + 1;
         }
-        ("".to_owned(), TokenizerCurrentState::Unknown)
+        CurrentState {
+            token: "".to_owned(),
+            state: TokenizerState::Unknown,
+        }
     }
 
     /// Advances `index` till it reaches the first char that doesn't match with `\s`(any whitespace char).
@@ -89,66 +97,96 @@ pub mod tokenizer_mod {
         }
     }
 
-    fn proceed_from_open_angle_bracket(
-        markup: &Vec<char>,
-        index: &mut usize,
-    ) -> (String, TokenizerCurrentState) {
+    fn proceed_from_open_angle_bracket(markup: &Vec<char>, index: &mut usize) -> CurrentState {
         let max = markup.len();
         let mut tag_name = String::from("");
 
         update_starting_tag_index(index, max, markup);
         if *index == max {
-            return (
-                "".to_owned(),
-                TokenizerCurrentState::Error(
+            return CurrentState {
+                token: "".to_owned(),
+                state: TokenizerState::Error(
                     "No tag name was found after open angle bracket.".to_owned(),
                 ),
-            );
+            };
         }
 
         update_starting_tag_name(index, &mut tag_name, markup);
         let collected_tag_name: Vec<char> = tag_name.chars().collect();
         let is_valid_string = collected_tag_name.iter().all(|x| x.is_alphabetic());
         if !is_valid_string {
-            return (
-                "".to_owned(),
-                TokenizerCurrentState::Error(
+            return CurrentState {
+                token: "".to_owned(),
+                state: TokenizerState::Error(
                     "Provided tag name '{tag_name}' contains invalid characters.".to_owned(),
                 ),
-            );
+            };
         }
         let first_letter = collected_tag_name[0];
         let is_uppercase = first_letter.is_uppercase();
         if is_uppercase {
-            return (tag_name, TokenizerCurrentState::Component);
+            return CurrentState {
+                token: tag_name,
+                state: TokenizerState::Component,
+            };
         } else {
-            return (tag_name, TokenizerCurrentState::Tag);
+            return CurrentState {
+                token: tag_name,
+                state: TokenizerState::Tag,
+            };
         }
     }
 
-    pub fn tokenizer(markup: String) -> impl FnMut() -> (String, TokenizerCurrentState) {
+    fn proceed_from_tag_name(markup: &Vec<char>, index: &mut usize) {
+        let max = markup.len();
+        // let mut key_value_pair
+        update_starting_tag_index(index, max, markup);
+        if markup[*index] == '>' {
+        } else if markup[*index] == '/' {
+        } else {
+        }
+    }
+    fn proceed_from_component_name(markup: &Vec<char>, index: &mut usize) {}
+
+    pub fn tokenizer(markup: String) -> impl FnMut() -> CurrentState {
         let mut current_index: usize = 0;
         let collected_markup: Vec<char> = markup.chars().collect();
-        let mut state: TokenizerCurrentState = TokenizerCurrentState::Uninitialized;
+        let mut state: TokenizerState = TokenizerState::Uninitialized;
         let next = move || match state {
-            TokenizerCurrentState::Uninitialized => {
+            TokenizerState::Uninitialized => {
                 println!("current index is: {current_index}");
-                let (token, state_) =
-                    proceed_from_uninitialized(&collected_markup, &mut current_index);
+                let CurrentState {
+                    token,
+                    state: state_,
+                } = proceed_from_uninitialized(&collected_markup, &mut current_index);
                 state = state_;
                 current_index += 1;
-                return (token, state.clone());
+                return CurrentState {
+                    token,
+                    state: state.clone(),
+                };
             }
-            TokenizerCurrentState::OpenAngleBracket => {
+            TokenizerState::OpenAngleBracket => {
                 println!("current index is: {current_index}");
-                let (token, state_) =
-                    proceed_from_open_angle_bracket(&collected_markup, &mut current_index);
+                let CurrentState {
+                    token,
+                    state: state_,
+                } = proceed_from_open_angle_bracket(&collected_markup, &mut current_index);
                 state = state_;
                 current_index += 1;
-                return (token, state.clone());
+                return CurrentState {
+                    token,
+                    state: state.clone(),
+                };
             }
-            TokenizerCurrentState::Finalized => ("".to_owned(), state.clone()),
-            _ => ("others".to_owned(), TokenizerCurrentState::Unknown),
+            TokenizerState::Finalized => CurrentState {
+                token: "".to_owned(),
+                state: state.clone(),
+            },
+            _ => CurrentState {
+                token: "others".to_owned(),
+                state: TokenizerState::Unknown,
+            },
         };
 
         next

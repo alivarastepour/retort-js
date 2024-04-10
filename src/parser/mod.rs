1577fdaef9239bdfe66aab24768b71eb129ffac6
alivarastepour <alivaraste@outlook.com>
2024-04-10 13:06:05 +0330

    completed js module resolving logic

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,43 +1,49 @@
 pub mod parser_mod {
 
-    // use crate::component::component_mod::Component;
     use crate::component::component_mod::Component;
-    use crate::file_util::file_util_mod::ParsedFile;
+    use crate::error::error_mod::Error;
+    use crate::presenter::presenter_mod::ParsedPresenter;
     use crate::tokenizer::tokenizer_mod::{tokenizer, CurrentState, TokenizerState};
-    use serde::{Deserialize, Serialize};
     use serde_wasm_bindgen::from_value;
     use std::collections::HashMap;
-    use std::future::IntoFuture;
     use wasm_bindgen_futures::JsFuture;
-    use web_sys::console::{log_0, log_1};
-    use web_sys::js_sys::Promise;
+    use web_sys::js_sys::{Function, Promise};
 
     use std::fmt::Display;
     use wasm_bindgen::prelude::*;
+
+    pub enum NodeType {
+        Component(Component), //component object
+        Tag(String),          // tag name
+        Text(String),         // text content
+    }
 
     #[wasm_bindgen(module = "/module_resolver/module_resolver.js")]
     extern "C" {
         fn module_resolver(path: &str) -> Promise;
     }
 
-    #[wasm_bindgen]
-    pub async fn call_module_resolver(path: &str) -> Result<Component, serde_wasm_bindgen::Error> {
-        // Call the JavaScript function
-        log_1(&JsValue::from_str("here1"));
-        let promise = module_resolver(path);
-        log_1(&JsValue::from_str("here2"));
+    pub async fn call_module_resolver(path: &str) -> Result<Component, Error> {
+        let path = path.replace("\"", "").replace(";", "");
+        let promise = module_resolver(&path);
         let future = JsFuture::from(promise);
-        log_1(&JsValue::from_str("here3"));
-        let result = future.await.unwrap();
-        log_1(&JsValue::from_str("here4"));
-        let result: Result<Component, serde_wasm_bindgen::Error> = from_value(result);
-        log_1(&JsValue::from_str("here5"));
-        return result;
-    }
-    pub enum NodeType {
-        Component(Component), //component object
-        Tag(String),          // tag name
-        Text(String),         // text content
+        let result = future.await;
+        if let Result::Err(err) = &result {
+            let msg = err.as_string();
+            if let Option::None = msg {
+                let msg = "Expected an error message to be string, but wasn't.".to_owned();
+                return Err(Error::TypeError(msg));
+            }
+            let msg = msg.unwrap();
+            return Err(Error::ResolveError(msg));
+        }
+        let result = result.unwrap();
+        let component_result: Result<Component, serde_wasm_bindgen::Error> = from_value(result);
+        if let Result::Err(err) = component_result {
+            return Err(Error::SerdeWasmBindgenError(err));
+        }
+        let component = component_result.unwrap();
+        Ok(component)
     }
 
     impl Display for NodeType {
@@ -56,11 +62,30 @@ pub mod parser_mod {
         pub children: Vec<VirtualNode>,
     }
 
-    // TODOS:
-    // 1- empty strings are being tokenized as Texts, not cool.
+    impl Clone for VirtualNode {
+        fn clone(&self) -> Self {
+            Self {
+                attributes: self.attributes.clone(),
+                children: self.children.clone(),
+                node_type: NodeType::Tag("".to_owned()),
+            }
+        }
+    }
 
-    pub fn parse_vdom_from_string(parsed_file: ParsedFile) -> Vec<VirtualNode> {
-        let ParsedFile { imports, markup } = parsed_file;
+    fn get_parser_return_value(stack: Vec<VirtualNode>) -> Result<VirtualNode, Error> {
+        if stack.len() == 1 {
+            let top = stack.get(0).unwrap();
+            return Ok(top.clone());
+        }
+        let msg = "Presenter of each component must be wrapped inside one and only one wrapper."
+            .to_owned();
+        return Err(Error::ParsingError(msg));
+    }
+
+    pub async fn parse_vdom_from_string(
+        parsed_file: ParsedPresenter,
+    ) -> Result<VirtualNode, Error> {
+        let ParsedPresenter { imports, markup } = parsed_file;
         let mut get_next_token = tokenizer(markup);
         let mut stack: Vec<VirtualNode> = Vec::new();
         let mut stack_size: usize = 0;
@@ -69,17 +94,14 @@ pub mod parser_mod {
             let CurrentState { state, token } = get_next_token();
             match state {
                 TokenizerState::Finalized => {
-                    break;
+                    return get_parser_return_value(stack);
                 }
                 TokenizerState::Error(err) => {
-                    panic!("[ERROR] {err}")
+                    let err = format!("[ERROR] {err}");
+                    return Err(Error::ParsingError(err));
                 }
-                TokenizerState::ClosingAngleBracket => {}
-                TokenizerState::OpenAngleBracket => {}
                 TokenizerState::TagNameClose => {
                     let completed_node = stack.pop().unwrap();
-
-                    // vdom.push(completed_node);
                     stack_size -= 1;
                     if stack_size != 0 {
                         let parent_node = stack.get_mut(stack_size - 1).unwrap();
@@ -119,16 +141,22 @@ pub mod parser_mod {
                 TokenizerState::Component => {
                     let component_path = imports.get(&token);
                     if let Option::None = component_path {
-                        panic!(
-                            "{}",
-                            format!(
+                        let msg =
                             "An import statement for {token} was supposed to exist, but it didn't."
-                        )
-                        )
+                                .to_owned();
+                        return Err(Error::ReferenceError(msg));
                     }
                     let component_path = component_path.unwrap();
-                    log_1(&JsValue::from_str("here??"));
-                    let component = call_module_resolver(&component_path);
+                    let component = call_module_resolver(&component_path).await;
+                    if let Result::Err(err) = component {
+                        return Err(err);
+                    }
+                    let component = component.unwrap();
+                    stack.push(VirtualNode {
+                        attributes: HashMap::new(),
+                        children: Vec::new(),
+                        node_type: NodeType::Component(component),
+                    })
                 } // note that all Components are assumed to be self-closing at this point. The other variant is not handled
                 TokenizerState::Props => {
                     let owner_node = stack.get_mut(stack_size - 1).unwrap();
@@ -140,7 +168,6 @@ pub mod parser_mod {
                 }
                 TokenizerState::SelfClosingAngleBracket => {
                     let completed_node = stack.pop().unwrap();
-                    // vdom.push(completed_node);
                     stack_size -= 1;
                     if stack_size != 0 {
                         let parent_node = stack.get_mut(stack_size - 1).unwrap();
@@ -155,6 +182,6 @@ pub mod parser_mod {
                 _ => {}
             }
         }
-        return vdom;
+        return Err(Error::ParsingError("()".to_owned()));
     }
 }

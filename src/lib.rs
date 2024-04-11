8686babed1c9da5222ba0735f9f0a02de924ebfb
alivarastepour <alivaraste@outlook.com>
2024-04-11 14:30:58 +0330

    added a util module for util functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod component;
+mod dom;
 mod error;
 mod parser;
 mod presenter;
 mod tokenizer;
+mod util;
 pub mod lib_mod {}
---
+++ src/util/mod.rs
@@ -0,0 +1,15 @@
+pub mod util_mod {
+    pub fn option_has_value<T>(op: &Option<T>) -> bool {
+        if let Option::None = op {
+            return false;
+        };
+        return true;
+    }
+
+    pub fn result_is_ok<T, E>(rs: &Result<T, E>) -> bool {
+        if let Result::Err(_) = rs {
+            return false;
+        };
+        return true;
+    }
+}

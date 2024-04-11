05a7346f5cb43598bbdfefe5c6b6c95fe6b899bb
alivarastepour <alivaraste@outlook.com>
2024-04-11 22:53:47 +0330

    added some test scenarios for `proceed_from_uninitialized` function

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,6 +1,38 @@
-#[cfg(test)]
 pub mod test_mod {
+    #[cfg(test)]
     mod tokenizer_mod_test {
-        use crate::tokenizer::tokenizer_mod;
+        use crate::tokenizer::tokenizer_mod::{
+            proceed_from_uninitialized, CurrentState, TokenizerState,
+        };
+        #[test]
+        fn test_empty_markup() {
+            let markup: Vec<char> = " ".chars().collect();
+            let mut index = 0usize;
+            let CurrentState { state, token } = proceed_from_uninitialized(&markup, &mut index);
+            match state {
+                TokenizerState::Finalized => {
+                    assert!(token == "")
+                }
+                _ => {
+                    assert!(false)
+                }
+            }
+        }
+
+        #[test]
+        fn test_text_markup() {
+            let markup_string = "This is a plain test";
+            let markup: Vec<char> = markup_string.chars().collect();
+            let mut index = 0usize;
+            let CurrentState { state, token } = proceed_from_uninitialized(&markup, &mut index);
+            match state {
+                TokenizerState::Finalized => {
+                    assert!(token == markup_string)
+                }
+                _ => {
+                    assert!(false)
+                }
+            }
+        }
     }
 }

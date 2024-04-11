9315045357fb25cd1c84504b91f7851fb8e86040
alivarastepour <alivaraste@outlook.com>
2024-04-11 22:28:29 +0330

    inits of test module

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ mod component;
 mod error;
 mod parser;
 mod presenter;
+mod test;
 mod tokenizer;
 pub mod lib_mod {}
---
+++ src/test/mod.rs
@@ -0,0 +1,6 @@
+#[cfg(test)]
+pub mod test_mod {
+    mod tokenizer_mod_test {
+        use crate::tokenizer::tokenizer_mod;
+    }
+}

131a647c972571f76dcacd425b13e1b09ef81da3
alivarastepour <alivaraste@outlook.com>
2024-05-20 19:56:37 +0330

    resolves #43

--- src/evaluator/util.rs
+++ src/evaluator/util.rs
@@ -9,7 +9,7 @@ pub mod evaluator_util {
         return open_c == close_c;
     }
 
-    /// Performs a check on the wrapper chars of value of the attributes'.
+    /// Given a trimmed string input, checks whether it is wrapped inside curly brackets or not.
     pub fn is_a_valid_attribute_value(text: &str) -> bool {
         text.starts_with("{") && text.ends_with("}")
     }
